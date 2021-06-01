0f02f1b673e5b52c0dfaae80cc06d35a62a7565d
Chadley <chadley.davis@revature.net>
2021-06-01 16:40:32 -0500

    Project 1 - Simple Console App

---
+++ my-first-hello-world/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace my_first_hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How old are you (in years)?");
+            string age = Console.ReadLine();
+            Console.WriteLine("What is your name?");
+            string name = Console.ReadLine();
+            Console.WriteLine($"{name} is {age} years old.");
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
